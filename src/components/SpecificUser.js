5ad237d963c78e0deed234750df5ad95b8021a8e
coryortega <coryortega3@gmail.com>
2019-11-21 10:10:14 -0800

    minor changes

--- src/components/SpecificUser.js
+++ src/components/SpecificUser.js
@@ -39,14 +39,12 @@ export default class SpecificUser extends React.Component {
 
 
   render() {
-    console.log(this.state.user)
     if (!this.state.user) {
       return <div>Loading user information...</div>;
     }
 
     return (
               <div>
-                  {console.log(this.state.favorites)}
               <div className="name-pic">
                   <h2>{this.state.user.username}</h2>
                   <img src='https://genslerzudansdentistry.com/wp-content/uploads/2015/11/anonymous-user.png'></img>
@@ -57,7 +55,6 @@ export default class SpecificUser extends React.Component {
               </div>
               <h3>Top Nine</h3>
               {this.state.favorites.map(favorite => (
-            //    <FavoriteCard favorites={favorites} setFavorites={setFavorites} favorite={favorite} key={favorite.id}/>
               <p>{favorite.name}</p>
              ))}
           </div>
